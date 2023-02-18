8a4d5a1cddfa46ff2d6427ae159d931272b0446f
cyc <1343191733@qq.com>
2023-02-18 16:16:58 +0800

    第二次提交

--- leyou-gateway/src/main/java/com/leyou/gateway/config/JwtProperties.java
+++ leyou-gateway/src/main/java/com/leyou/gateway/config/JwtProperties.java
@@ -1,4 +1,4 @@
-package com.leyou.auth.config;
+package com.leyou.gateway.config;
 
 import com.leyou.common.utils.RsaUtils;
 import org.slf4j.Logger;
@@ -6,26 +6,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
-import java.security.PrivateKey;
 import java.security.PublicKey;
 
 @ConfigurationProperties(prefix = "leyou.jwt")
 public class JwtProperties {
 
-    private String secret; // 密钥
-
     private String pubKeyPath;// 公钥
-
-    private String priKeyPath;// 私钥
-
-    private int expire;// token过期时间
 
     private String cookieName;
 
     private PublicKey publicKey; // 公钥
-
-    private PrivateKey privateKey; // 私钥
 
     private static final Logger logger = LoggerFactory.getLogger(JwtProperties.class);
 
@@ -35,27 +25,12 @@ public class JwtProperties {
     @PostConstruct
     public void init(){
         try {
-            File pubKey = new File(pubKeyPath);
-            File priKey = new File(priKeyPath);
-            if (!pubKey.exists() || !priKey.exists()) {
-                // 生成公钥和私钥
-                RsaUtils.generateKey(pubKeyPath, priKeyPath, secret);
-            }
             // 获取公钥和私钥
             this.publicKey = RsaUtils.getPublicKey(pubKeyPath);
-            this.privateKey = RsaUtils.getPrivateKey(priKeyPath);
         } catch (Exception e) {
-            logger.error("初始化公钥和私钥失败！", e);
+            logger.error("初始化公钥失败！", e);
             throw new RuntimeException();
         }
-    }
-
-    public String getSecret() {
-        return secret;
-    }
-
-    public void setSecret(String secret) {
-        this.secret = secret;
     }
 
     public String getPubKeyPath() {
@@ -64,22 +39,6 @@ public class JwtProperties {
 
     public void setPubKeyPath(String pubKeyPath) {
         this.pubKeyPath = pubKeyPath;
-    }
-
-    public String getPriKeyPath() {
-        return priKeyPath;
-    }
-
-    public void setPriKeyPath(String priKeyPath) {
-        this.priKeyPath = priKeyPath;
-    }
-
-    public int getExpire() {
-        return expire;
-    }
-
-    public void setExpire(int expire) {
-        this.expire = expire;
     }
 
     public String getCookieName() {
@@ -98,17 +57,7 @@ public class JwtProperties {
         this.publicKey = publicKey;
     }
 
-    public PrivateKey getPrivateKey() {
-        return privateKey;
-    }
-
-    public void setPrivateKey(PrivateKey privateKey) {
-        this.privateKey = privateKey;
-    }
-
     public static Logger getLogger() {
         return logger;
     }
-
-    // getter setter ...
 }
